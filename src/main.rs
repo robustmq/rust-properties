bfeab77f495a70348df6106b2cc4049ecda2055c
Justin Liu <liuguanj@outlook.com>
2023-09-08 00:32:16 +0800

    update lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,34 @@
 use std::env;
-use std::fs;
+use std::process;
+use rust_properties::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    println!("File name is : {}", file_path);
-    let contents = fs::read_to_string(file_path).expect("should have been able to read the file ");
-    println!("With text: \n{contents}");
+
+   let config = Config::build(&args).unwrap_or_else(|err|{
+    println!("Problem parsing arguments: {err}");
+    process::exit(1);
+   });
+   
+    println!("searching for : {}", config.query);
+    println!("searching for : {}", config.file_path);
+    if let Err(e) = rust_properties::run(config){
+        println!("Application error: {e}");
+        process::exit(1);
+    }
    // dbg!(args);
    // println!("Hello, world!");
 }
+
+// fn parse_config(args: &[String]) -> Result<&str, &'static str> {
+
+//     if args.len() < 2 {
+//        // panic!("not enough arguments, you may miss the file path");
+    
+//        return Err("not enough arguments, you may miss the file path");
+//     }
+//     let file_path = &args[1];
+//     Ok(&file_path)
+// }
+
+
